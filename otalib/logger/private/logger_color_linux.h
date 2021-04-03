66ee2f92338cf0feeca97530d2be8cdbcf36382e
josexy <josephxrays@163.com>
2021-04-03 16:20:04 +0800

    private file based on windows/linux

---
+++ otalib/logger/private/logger_color_linux.h
@@ -0,0 +1,87 @@
+#ifndef LOGGER_COLOR_LINUX_H
+#define LOGGER_COLOR_LINUX_H
+
+#include <string>
+
+namespace color {
+enum class ForegroundColor {
+    None = 0,
+    Black = 30,
+    Red = 31,
+    Green = 32,
+    Orange = 33,
+    Yellow = 33,
+    Blue = 34,
+    Solferino = 35,
+    Cyan = 36,
+    LightGray = 37
+};
+enum class BackgroundColor {
+    None = 0,
+    Black = 40,
+    Red = 41,
+    Green = 42,
+    Orange = 43,
+    Blue = 44,
+    Solferino = 45,
+    Cyan = 46,
+    LightGray = 47,
+    DeepGray = 100,
+    LightRed = 101,
+    LightGreen = 102,
+    Yellow = 103,
+    LightBlue = 104,
+    LightPurple = 105,
+    BlueGreen = 106,
+    White = 107
+};
+
+enum class ControlCode {
+    None = 0,
+    Highlight = 1,
+    LessHighlight = 2,
+    Italic = 3,
+    UnderLine = 4,
+};
+
+}  // namespace color
+using fgColor = color::ForegroundColor;
+using bgColor = color::BackgroundColor;
+using ctlColor = color::ControlCode;
+
+class logger_color {
+   public:
+    explicit logger_color(fgColor fgcolor = fgColor::None,
+                          bgColor bgcolor = bgColor::None,
+                          ctlColor ctlcolor = ctlColor::None) {
+        prefix_.append("\033[");
+        bool last_ = false;
+        if (fgcolor != fgColor::None) {
+            last_ = true;
+            prefix_.append(std::to_string(static_cast<int>(fgcolor)));
+        }
+        if (bgcolor != bgColor::None) {
+            if (last_) prefix_.append(";");
+            prefix_.append(std::to_string(static_cast<int>(bgcolor)));
+            last_ = true;
+        }
+        if (ctlcolor != ctlColor::None) {
+            if (last_) prefix_.append(";");
+            prefix_.append(std::to_string(static_cast<int>(ctlcolor)));
+            last_ = true;
+        }
+        if (last_) prefix_.append("m");
+    }
+
+    std::string operator()(const std::string &text) {
+        std::string value = prefix_;
+        value.append(text);
+        value.append("\033[0m");
+        return value;
+    }
+
+   private:
+    std::string prefix_;
+};
+
+#endif
---
+++ otalib/logger/private/logger_color_win.h
@@ -0,0 +1,83 @@
+#ifndef LOGGER_COLOR_WIN_H
+#define LOGGER_COLOR_WIN_H
+
+#include <windows.h>
+
+#include <iostream>
+#include <string>
+namespace color {
+enum class FgColor {
+    None = 0,
+    Red = FOREGROUND_RED,
+    Green = FOREGROUND_GREEN,
+    Blue = FOREGROUND_BLUE,
+    White = Red | Green | Blue,
+    Black = ~White,
+    Yellow = Red | Green,
+    Other1 = Red | Blue,
+    Other2 = Green | Blue
+};
+
+enum class BgColor {
+    None = 0,
+    Red = BACKGROUND_RED,
+    Green = BACKGROUND_GREEN,
+    Blue = BACKGROUND_BLUE,
+    White = Red | Green | Blue,
+    Black = ~White,
+    Yellow = Red | Green,
+    Other1 = Red | Blue,
+    Other2 = Green | Blue
+};
+
+}  // namespace color
+using fgColor = color::FgColor;
+using bgColor = color::BgColor;
+
+class logger_color {
+   public:
+    explicit logger_color(fgColor fgcolor, bgColor bgcolor,
+                          bool highlight = true)
+        : newAttr_{0} {
+        handle_ = ::GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO scbi;
+        ::GetConsoleScreenBufferInfo(handle_, &scbi);
+        oldAttr_ = scbi.wAttributes;
+
+        if (highlight) {
+            newAttr_ |= FOREGROUND_INTENSITY;
+        }
+
+        setFg(fgcolor);
+        setBg(bgcolor);
+        // newAttr_ |= BACKGROUND_INTENSITY;
+    }
+
+    std::string operator()(const std::string &text) { return text; }
+    static void reset() {
+        SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), oldAttr_);
+    }
+
+   private:
+    void setFg(fgColor foreground_color) {
+        newAttr_ |= static_cast<WORD>(foreground_color);
+        SetConsoleTextAttribute(handle_, newAttr_);
+    }
+
+    void setBg(bgColor background_color) {
+        newAttr_ |= static_cast<WORD>(background_color);
+        SetConsoleTextAttribute(handle_, newAttr_);
+    }
+
+    void setV(WORD color_value) {
+        newAttr_ = (WORD)color_value;
+        SetConsoleTextAttribute(handle_, newAttr_);
+    }
+
+   private:
+    HANDLE handle_;
+    static WORD oldAttr_;
+    WORD newAttr_;
+};
+WORD logger_color::oldAttr_ = 0;
+#endif
