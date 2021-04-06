d51fc69777bdb22e949e777c4c63f1b2fc4bb757
THF5000 <Hyliang2000@e.gzhu.edu.cn>
2021-04-06 13:17:21 +0800

    Static variable initialization bug fixed.
    Also add namespace otalib

--- otalib/logger/private/logger_color_win.h
+++ otalib/logger/private/logger_color_win.h
@@ -9,27 +9,27 @@
 namespace otalib {
 namespace color {
 enum class FgColor {
-    None = 0,
-    Red = FOREGROUND_RED,
-    Green = FOREGROUND_GREEN,
-    Blue = FOREGROUND_BLUE,
-    White = Red | Green | Blue,
-    Black = ~White,
-    Yellow = Red | Green,
-    Other1 = Red | Blue,
-    Other2 = Green | Blue
+  None = 0,
+  Red = FOREGROUND_RED,
+  Green = FOREGROUND_GREEN,
+  Blue = FOREGROUND_BLUE,
+  White = Red | Green | Blue,
+  Black = ~White,
+  Yellow = Red | Green,
+  Other1 = Red | Blue,
+  Other2 = Green | Blue
 };
 
 enum class BgColor {
-    None = 0,
-    Red = BACKGROUND_RED,
-    Green = BACKGROUND_GREEN,
-    Blue = BACKGROUND_BLUE,
-    White = Red | Green | Blue,
-    Black = ~White,
-    Yellow = Red | Green,
-    Other1 = Red | Blue,
-    Other2 = Green | Blue
+  None = 0,
+  Red = BACKGROUND_RED,
+  Green = BACKGROUND_GREEN,
+  Blue = BACKGROUND_BLUE,
+  White = Red | Green | Blue,
+  Black = ~White,
+  Yellow = Red | Green,
+  Other1 = Red | Blue,
+  Other2 = Green | Blue
 };
 
 }  // namespace color
@@ -37,50 +37,48 @@ using fgColor = color::FgColor;
 using bgColor = color::BgColor;
 
 class logger_color {
-   public:
-    explicit logger_color(fgColor fgcolor, bgColor bgcolor,
-                          bool highlight = true)
-        : newAttr_{0} {
-        handle_ = ::GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_SCREEN_BUFFER_INFO scbi;
-        ::GetConsoleScreenBufferInfo(handle_, &scbi);
-        oldAttr_ = scbi.wAttributes;
+ public:
+  explicit logger_color(fgColor fgcolor, bgColor bgcolor, bool highlight = true)
+      : newAttr_{0} {
+    handle_ = ::GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO scbi;
+    ::GetConsoleScreenBufferInfo(handle_, &scbi);
+    oldAttr_ = scbi.wAttributes;
 
-        if (highlight) {
-            newAttr_ |= FOREGROUND_INTENSITY;
-        }
-
-        setFg(fgcolor);
-        setBg(bgcolor);
-        // newAttr_ |= BACKGROUND_INTENSITY;
+    if (highlight) {
+      newAttr_ |= FOREGROUND_INTENSITY;
     }
 
-    std::string operator()(const std::string &text) { return text; }
-    static void reset() {
-        SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), oldAttr_);
-    }
+    setFg(fgcolor);
+    setBg(bgcolor);
+    // newAttr_ |= BACKGROUND_INTENSITY;
+  }
 
-   private:
-    void setFg(fgColor foreground_color) {
-        newAttr_ |= static_cast<WORD>(foreground_color);
-        SetConsoleTextAttribute(handle_, newAttr_);
-    }
+  std::string operator()(const std::string &text) { return text; }
+  static void reset() {
+    SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), oldAttr_);
+  }
 
-    void setBg(bgColor background_color) {
-        newAttr_ |= static_cast<WORD>(background_color);
-        SetConsoleTextAttribute(handle_, newAttr_);
-    }
+ private:
+  void setFg(fgColor foreground_color) {
+    newAttr_ |= static_cast<WORD>(foreground_color);
+    SetConsoleTextAttribute(handle_, newAttr_);
+  }
 
-    void setV(WORD color_value) {
-        newAttr_ = (WORD)color_value;
-        SetConsoleTextAttribute(handle_, newAttr_);
-    }
+  void setBg(bgColor background_color) {
+    newAttr_ |= static_cast<WORD>(background_color);
+    SetConsoleTextAttribute(handle_, newAttr_);
+  }
 
-   private:
-    HANDLE handle_;
-    static WORD oldAttr_;
-    WORD newAttr_;
+  void setV(WORD color_value) {
+    newAttr_ = (WORD)color_value;
+    SetConsoleTextAttribute(handle_, newAttr_);
+  }
+
+ private:
+  HANDLE handle_;
+  static inline WORD oldAttr_ = 0;
+  WORD newAttr_;
 };
-}
-WORD logger_color::oldAttr_ = 0;
+}  // namespace otalib
 #endif
