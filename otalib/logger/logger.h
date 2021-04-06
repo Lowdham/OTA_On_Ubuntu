09601cf03e50434ba605d0331820ddb90bb7e61e
THF5000 <56789071+Lowdham@users.noreply.github.com>
2021-04-06 22:22:24 +0800

    Update logger.h

--- otalib/logger/logger.h
+++ otalib/logger/logger.h
@@ -10,112 +10,16 @@
 #include "logger_color.h"
 #include "utils.hpp"
 
-#define __DEBUG 1
-#define __LOGGER_DATETIME 1
-
 namespace otalib {
-namespace logger {
-
-enum class Level {
-  K_INFO = 0x01,
-  K_ERROR = 0x02,
-  K_WARN = 0x03,
-  K_DEBUG = 0x04
-};
-
-class Logger {
- public:
-  static Logger& getStream(Level level) {
-    static Logger logger;
-    logger.inner_ss_.str("");
-    logger.level_ = level;
-    logger.first_ = false;
-    std::cout.clear();
-    std::cout.flush();
-    return logger;
-  };
-
-  void log() { std::cout << std::endl; }
-
-  template <class T>
-  void log(const T& val) {
-    appendTag();
-    inner_ss_ << val;
-    bool __normal__ = true, __debug__ = false;
-    std::string text = inner_ss_.str();
-#ifdef __LOGGER_COLOR
-    if (level_ == Level::K_ERROR)
-      text = LOGGER_COLOR(fgColor::Red, bgColor::None).operator()(text);
-    else if (level_ == Level::K_DEBUG) {
-      text = LOGGER_COLOR(fgColor::Red, bgColor::None).operator()(text);
-      __normal__ = false;
-#ifdef __DEBUG
-      __debug__ = true;
-#endif
-    } else if (level_ == Level::K_INFO)
-      text = LOGGER_COLOR(fgColor::Green, bgColor::None).operator()(text);
-    else if (level_ == Level::K_WARN)
-      text = LOGGER_COLOR(fgColor::Yellow, bgColor::None).operator()(text);
-#endif
-    if (__normal__ || __debug__) std::cout << text << std::endl;
-    // restore the attribute of console on Windows
-    LOGGER_COLOR_RESET
-  }
-
-  template <class T, class... Args>
-  void log(const T& val, Args... args) {
-    appendTag();
-    inner_ss_ << val << " ";
-    log(args...);
-  }
-
- private:
-  void appendTag() {
-    if (!first_) {
-      std::string level_label;
-
-#ifdef __LOGGER_DATETIME
-      char buf[64]{0};
-      time_t t = time(nullptr);
-      strftime(buf, 64, "%Y-%m-%d %H:%M:%S", localtime(&t));
-      level_label = std::string(buf) + " ";
-#endif
-
-      switch (level_) {
-        case Level::K_INFO:
-          level_label += "[INFO]";
-          break;
-        case Level::K_ERROR:
-          level_label += "[ERROR]";
-          break;
-        case Level::K_WARN:
-          level_label += "[WARN]";
-          break;
-        case Level::K_DEBUG:
-          level_label += "[DEBUG]";
-          break;
-        default:
-          level_label += "[DEFAULT]";
-          break;
-      }
-
-      inner_ss_ << level_label << " ";
-      first_ = true;
-    }
-  }
-
- private:
-  std::stringstream inner_ss_;
-  bool first_;
-  Level level_;
-};
-}  // namespace logger
 
 static constexpr char kHeadTagFatalError[] = "[FATAL ERROR]";
+static constexpr char kHeadTagError[] = "[FATAL ERROR]";
 static constexpr char kHeadTagDebug[] = "[DEBUG]";
+static constexpr char kHeadTagWarn[] = "[WARNNING]";
+static constexpr char kHeadTagInfo[] = "[INFO]";
 
 template <auto HeadTag, auto Sep, fgColor fcolor,
-          bool otime = true, bool linefeed = true, bool thread_safe = false>
+          bool otime = false, bool linefeed = true, bool thread_safe = false>
 struct PrintCtrl {
   static constexpr decltype(auto) tag() noexcept { return HeadTag; }
   static constexpr decltype(auto) sep() noexcept { return Sep; }
@@ -125,7 +29,11 @@ struct PrintCtrl {
   static constexpr bool isThreadSafe() noexcept { return thread_safe; }
 };
 
-using GeneralDebugCtrl = PrintCtrl<kHeadTagDebug, ' ', fgColor::Red, true>;
+using GeneralFerrorCtrl = PrintCtrl<kHeadTagFatalError, ' ', fgColor::Red>;
+using GeneralErrorCtrl = PrintCtrl<kHeadTagError, ' ', fgColor::Red>;
+using GeneralDebugCtrl = PrintCtrl<kHeadTagDebug, ' ', fgColor::Yellow>;
+using GeneralWarnCtrl = PrintCtrl<kHeadTagWarn, ' ', fgColor::Yellow>;
+using GeneralInfoCtrl = PrintCtrl<kHeadTagInfo, ' ', fgColor::None>;
 
 template <typename ctrl, typename OutputStream, typename... Args>
 void print(OutputStream&& out, Args&&... args) {
@@ -178,12 +86,5 @@ inline std::ostream& operator<<(std::ostream& out, QString&& str) {
   out << str.toStdString();
   return out;
 }
-
-#define LOG_LEVEL(LEVEL, ...) \
-  otalib::logger::Logger::getStream((LEVEL)).log(__VA_ARGS__);
-#define LOG_INFO(...) LOG_LEVEL(otalib::logger::Level::K_INFO, __VA_ARGS__)
-#define LOG_ERROR(...) LOG_LEVEL(otalib::logger::Level::K_ERROR, __VA_ARGS__)
-#define LOG_WARN(...) LOG_LEVEL(otalib::logger::Level::K_WARN, __VA_ARGS__)
-#define LOG_DEBUG(...) LOG_LEVEL(otalib::logger::Level::K_DEBUG, __VA_ARGS__)
 
 #endif
