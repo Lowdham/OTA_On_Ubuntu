9aef3b1d41000d0cb7e8ccca64a167f28f44e152
THF5000 <Hyliang2000@e.gzhu.edu.cn>
2021-04-15 21:09:31 +0800

    perfect the testvcm()

--- test/vcm_test/vcmtest.hpp
+++ test/vcm_test/vcmtest.hpp
@@ -3,29 +3,59 @@
 
 using namespace otalib;
 
+GeneralVersion randomGen() {
+  int _1 = rand() % 7;
+  int _2 = rand() % 10;
+  int _3 = rand() % 9 + 1;
+  QString info = QString::number(_1) + "." + QString::number(_2) + "." +
+                 QString::number(_3);
+  return GeneralVersion(info);
+}
+
 void test_vcm() {
-  srand(time(0));
-  VersionMap<int> vm;
-  for (int i = 0; i <= 500000; i++) vm.append(i, true);
-  auto r = vm.search<SearchStrategy::vUpdate>(203824, 454300);
-  if (r.empty()) {
-    print<GeneralWarnCtrl>(std::cout, "Not Found!");
-    return;
+  print<GeneralInfoCtrl>(
+      std::cout,
+      "Mid's capacity:" + std::to_string(VcmControl<VCM::Mid>::vcm_capacity));
+
+  VersionMap<GeneralVersion> vm;
+  GeneralVersion vbase("0.0.1");
+  auto vcur = vbase;
+  for (uint64_t i = 0; i <= GeneralVersion::vcm_capacity; i++) {
+    vm.append(vcur, true);
+    vcur = GeneralVersion::add(vcur, vbase);
   }
 
-  size_t update_size = 0;
-  size_t rollback_size = 0;
-  for (auto& y : r) {
-    if (y.first < y.second)
-      ++update_size;
-    else
-      ++rollback_size;
-    std::cout << y.first << "->" << y.second << " ";
+  auto start = clock();
+  auto end = clock();
+  auto during = end;
+  uint64_t success = 0;
+  uint64_t fail = 0;
+  std::vector<std::pair<GeneralVersion, GeneralVersion>> fails;
+  for (uint64_t i = 0; i < 100000; ++i) {
+    auto s = randomGen();
+    GeneralVersion d;
+    while (true) {
+      d = randomGen();
+      if (s != d) break;
+    }
+    start = clock();
+    auto r = vm.search<SearchStrategy::vUpdate>(s, d);
+    end = clock();
+    during += end - start;
+    if (r.empty()) {
+      ++fail;
+      fails.emplace_back(s, d);
+    } else
+      ++success;
   }
-  auto size = r.size();
-  std::cout << "\n";
-  print<GeneralSuccessCtrl>(std::cout, "Package:" + std::to_string(size));
-  print<GeneralInfoCtrl>(std::cout, "Update:" + std::to_string(update_size));
+  auto sec = (static_cast<double>(during) / CLOCKS_PER_SEC);
   print<GeneralInfoCtrl>(std::cout,
-                         "Rollback:" + std::to_string(rollback_size));
+                         "Total cost: " + QString::number(sec) + " seconds");
+  print<GeneralSuccessCtrl>(std::cout,
+                            "Total success: " + QString::number(success));
+  print<GeneralErrorCtrl>(std::cout, "Total fail: " + QString::number(fail));
+  print<GeneralErrorCtrl>(std::cout, "Failed path: ");
+  for (auto i : fails)
+    print<GeneralErrorCtrl>(std::cout, "[" + i.first.toString() + "]->[" +
+                                           i.second.toString() + "]");
 }
