9147161af923324d10d420489e4cdd438d1b790c
joxrays <30660121+josexy@users.noreply.github.com>
2021-05-25 12:28:11 +0800

    Revert "Update"
    This reverts commit 03bfe50e224eb97ded13938bf577c25d1a9cd7e0.

--- server/include/FileLoader.hpp
+++ server/include/FileLoader.hpp
@@ -1,11 +1,7 @@
 #ifndef OTASERVER_FILELOADER_HPP
 #define OTASERVER_FILELOADER_HPP
 
-#include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <unistd.h>
 
 #include <string>
 
@@ -16,7 +12,7 @@ namespace net {
 
 class FileLoader {
  public:
-  explicit FileLoader(const std::string &filename) : file_name_(filename) {
+  explicit FileLoader(const std::string &filename) {
     fp_ = ::fopen(filename.c_str(), "rb");
   }
 
@@ -25,28 +21,18 @@ class FileLoader {
     fp_ = nullptr;
   }
 
-  //  void readAll(net::Buffer *buffer) {
-  //    if (!fp_) return;
-  //    ::fseek(fp_, 0, SEEK_END);
-  //    auto size = ::ftell(fp_);
-  //    ::rewind(fp_);
-  //    buffer->ensure_writable(size);
-  //    ::fread(buffer->begin_write(), sizeof(char), size, fp_);
-  //    buffer->has_written(size);
-  //  }
-
   void readAll(net::Buffer *buffer) {
-    int fd = ::open(file_name_.c_str(), O_RDONLY);
-    auto size = ::lseek(fd, 0, SEEK_END);
-
-    char *mapped = (char *)mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    ::close(fd);
-    buffer->set_mapped(mapped, size);
+    if (!fp_) return;
+    ::fseek(fp_, 0, SEEK_END);
+    auto size = ::ftell(fp_);
+    ::rewind(fp_);
+    buffer->ensure_writable(size);
+    ::fread(buffer->begin_write(), sizeof(char), size, fp_);
+    buffer->has_written(size);
   }
 
  private:
   FILE *fp_;
-  std::string file_name_;
 };
 }  // namespace net
 
